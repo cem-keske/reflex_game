b6437335885030d8ef6bacff5f82f341f6ccbafd
Ayla WATSUJI <aylawatsuji347@gmail.com>
2024-08-11 22:56:47 +0200

    display added

--- src/main/start.cpp
+++ src/main/start.cpp
@@ -1,8 +1,20 @@
 #include <Arduino.h>
 #include "start.h"
-
 #include "Push_Button.h"
 #include "Standard_Led.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+constexpr auto SCREEN_WIDTH  = 128; // OLED display width, in pixels
+constexpr auto SCREEN_HEIGHT = 64; // OLED display height, in pixels
+
+// Declaration for SSD1306 display connected using I2C
+constexpr auto OLED_RESET   =  -1; // Reset pin
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 Standard_Led red_leds[3] = { Standard_Led(32), Standard_Led(26), Standard_Led(14) };
 constexpr size_t no_of_leds = sizeof(red_leds) / sizeof(red_leds[0]);
@@ -10,6 +22,9 @@ Standard_Led yellow_leds[2] = { Standard_Led(25), Standard_Led(33) };
 Standard_Led green_led(13);
 Push_Button buttons[2] = {Push_Button(16, false), Push_Button(17, false)};
 bool flag = true;
+unsigned long reflex_time;
+
+
 
 void startGame(){
   for(auto & red_led : red_leds) {
@@ -35,19 +50,42 @@ void compete(){
     red_led.turn_off();
   }
   green_led.turn_on();
+  reflex_time = millis();
+
 }
 
 void winner_decleration(int winner) {
+  display.clearDisplay();
+  display.setCursor(0,2);
+  display.println(millis()-reflex_time);
   yellow_leds[winner].turn_on();
-  Serial.println("Winning player:");
-  Serial.println(winner);
+  display.println("Winning player:");
+  display.println(winner);
+  display.display();
   delay(3000);
   yellow_leds[winner].turn_off();
+
+  
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+   // initialize the OLED object
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Clear the buffer.
+  display.clearDisplay();
+
+  display.setTextSize(1,2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,32);
+  display.println("Hello world!");
+  display.display();
+  delay(10000);
 }
 
 void loop() {
